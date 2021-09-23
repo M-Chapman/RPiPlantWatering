781f1f45c717192dccd4d1b3f93d14b79ab6807f
Mike Chapman <ChapmanMA1@cardiff.ac.uk>
2021-09-23 14:28:31 +0100

    added tkinter gui to main program

--- RaspberryPiPlantWateringSystem.py
+++ RaspberryPiPlantWateringSystem.py
@@ -5,6 +5,7 @@ import datetime
 import time
 import csv
 import os
+import tkinter as tk
 import numpy as np
 import pandas as pd
 
@@ -14,7 +15,45 @@ spi = spidev.SpiDev()
 spi.open(0, 0)
 spi.max_speed_hz = 1000000
 
-# def setup():
+GUI = tk.Tk()
+GUI.title('Raspberry Pi Plant Watering System')
+GUI.geometry("800x600")
+
+frame = tk.Frame(GUI)
+frame.pack()
+
+setup = True
+
+
+def background():
+    # Background runs the soil moisture monitoring
+    # It changes the global variable setup to only run once and then begins monitoring soil moisture
+    global setup
+    global active_channels
+    global counter
+
+    if setup:
+        # Runs for first time only
+        active_channels = []
+        counter = 0
+        for i in range(7):
+            if get_moisture(i) > 2:
+                # Checks if a channel is active
+                create_table(i)
+                active_channels.append(i)
+                time.sleep(2)
+        setup = False
+        GUI.after(2000,background)
+    else:
+        # Runs after first time
+        for i in active_channels:
+            add_moisture(i, get_moisture(i))
+            time.sleep(2)
+    counter += 1
+    print('Background has run {} times'.format(counter))
+
+    GUI.after(2000,background)
+
 
 def get_status(pin):
     GPIO.setup(pin, GPIO.IN)
@@ -22,15 +61,16 @@ def get_status(pin):
 
 
 def get_moisture(channel):
-    #Returns the percentage soil moisture level as a float (0 to 100)
-    #Accepts an integer
+    # Returns the percentage soil moisture level as a float (0 to 100)
+    # Accepts an integer
     val = spi.xfer2([1, (8+channel) << 4, 0])
     data = ((val[1] & 3) << 8) + val[2]
     return round(data/10.23, 2)  # Outputs value as % of max value
 
+
 def create_table(channel):
-    #CSV file creation for soil moisture level
-    #Accepts an integer in the range 0 to 7
+    # CSV file creation for soil moisture level
+    # Accepts an integer in the range 0 to 7
     if 0 <= channel <= 7:
         fields = ['channel', 'Time', 'Moisture']
         filename = "csvfiles/moisturechannel{}.csv".format(channel)
@@ -38,51 +78,32 @@ def create_table(channel):
         with open(filename, 'w', newline='') as csvfile:
             csvwriter = csv.DictWriter(csvfile, fieldnames=fields)
             csvwriter.writeheader()
-            #csvwriter.writerow(fields)
+            # csvwriter.writerow(fields)
 
-            csvwriter.writerow({'channel': channel,'Time':  datetime.datetime.now().replace(microsecond = 0), 'Moisture': get_moisture(channel)})
+            csvwriter.writerow({'channel': channel, 'Time':  datetime.datetime.now(
+            ).replace(microsecond=0), 'Moisture': get_moisture(channel)})
     else:
-        print("The channel specified ({}) is outside the valid range (0 to 7)".format(channel))
+        print("The channel specified ({}) is outside the valid range (0 to 7)".format(
+            channel))
+
 
 def add_moisture(channel, value):
-    #Adds the soil moisture level to the csv file of the specified channel
-    #Accepts an integer and float
+    # Adds the soil moisture level to the csv file of the specified channel
+    # Accepts an integer and float
     fields = ['channel', 'Time', 'Moisture']
     with open("csvfiles/moisturechannel{}.csv".format(channel), 'a') as csvfile:
         csvwriter = csv.DictWriter(csvfile, fields)
-        csvwriter.writerow({'channel': channel, 'Time' : datetime.datetime.now().replace(microsecond = 0), 'Moisture': value})
+        csvwriter.writerow({'channel': channel, 'Time': datetime.datetime.now(
+        ).replace(microsecond=0), 'Moisture': value})
+
 
 def rename_file(current_name, new_name):
-    #Renames the specified .csv file to a new name
-    #Accepts 2 strings
-    os.rename("csvfiles/{}.csv".format(current_name), "csvfiles/{}.csv".format(new_name))
+    # Renames the specified .csv file to a new name
+    # Accepts 2 strings
+    os.rename("csvfiles/{}.csv".format(current_name),
+              "csvfiles/{}.csv".format(new_name))
 
-if __name__ == "__main__":
-    try:
-        active_channels = []
-        setup = True
-        while True:
 
-            if setup:
-                #Runs for first time only
-                for i in range(7):
-                    if get_moisture(i) > 2:
-                        #Checks if a channel is active
-                        create_table(i)
-                        active_channels.append(i)
-                    time.sleep(1)
-                setup = False
-            else:
-                #Runs after first time
-                for i in active_channels:
-                    add_moisture(i,get_moisture(i))
-                    time.sleep(1)
+GUI.after(2000, background)
 
-            # df = pd.read_csv("csvfiles/pin0.csv")
-            # df.plot()
-            # print(df)
-
-            time.sleep(5)
-
-    except KeyboardInterrupt:
-        print("Cancel.")
+GUI.mainloop()
--- TkinterTest.py
+++ TkinterTest.py
@@ -1,12 +1,28 @@
-import tkinter
+import tkinter as tk
 
-GUI = tkinter.Tk()
+GUI = tk.Tk()
+
+frame = tk.Frame(GUI)
+frame.pack()
+
+def BTask():
+    print("B has been pressed!")
+
 
 GUI.title('Raspberry Pi Plant Watering System')
-B = tkinter.Button(GUI, bg = 'grey', text='This is a Button')
+B = tk.Button(frame, fg = 'blue', text='This is a Button', command = BTask)
 
+BBot = tk.Button(frame, fg='red', text = 'Button 2')
+
+BBot.pack()
 B.pack()
 
 GUI.geometry("800x600")
+
+def task():
+    print('Hello World!')
+    GUI.after(2000,task)
+
+GUI.after(2000, task)
 
 GUI.mainloop()
