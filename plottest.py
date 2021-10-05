acccfb30dd2729bde841650729a1f3caa412302e
Mike Chapman <ChapmanMA1@cardiff.ac.uk>
2021-10-05 10:33:03 +0100

    first operational visualisation of dataframe created

--- plottest.py
+++ plottest.py
@@ -1,6 +1,12 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
 df = pd.read_csv("csvfiles/pin0.csv")
-df = pd.DataFrame(pd.read_csv("csvfiles/pin0.csv"))
-df.plot.line()
+df = pd.DataFrame(df, columns=['Time', 'Moisture'])
+
+print(len(df))
+
+df.plot(x='Time', y='Moisture', kind='line')
+
+plt.show()
